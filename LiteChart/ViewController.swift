503b390e3284c0f24e0c6f48bcd58483d415dc36
刘洋 <1269458422@qq.com>
2020-06-15 17:25:10 +0800

    fix(显示标签):替换UILabel
    采用绘制方法替换UILabel解决不自动适配高度的问题

--- LiteChart/ViewController.swift
+++ LiteChart/ViewController.swift
@@ -15,10 +15,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var configure = LiteChartViewParameters(inputDatas: .bar(inputDatas: [(.init(lightColor: .blueViolet), [1, 1, 2, 2])], coupleTitle: ["橘子", "苹果", "香蕉", "李子"]))
+        var configure = LiteChartViewParameters(inputDatas: .bar(inputDatas: [(.init(lightColor: .blueViolet), [1, 1, 2, 2]), (.init(lightColor: .hotPink), [1, 1, 2, 2])], coupleTitle: ["橘子", "苹果", "香蕉", "李子"]))
         configure.isShowValueDividingLine = true
         configure.isShowCoupleDividingLine = true
-//        configure.direction = .leftToRight
+        configure.direction = .leftToRight
+        configure.inputLegendTitles = ["山东", "湖北"]
+        configure.titleString = "水果产量"
         
         do {
             let backgroundView = try LiteChartView(configure: configure)
@@ -27,8 +29,8 @@ class ViewController: UIViewController {
             backgroundView.snp.makeConstraints{
                 make in
                 make.center.equalToSuperview()
-                make.width.equalTo(100)
-                make.height.equalTo(100)
+                make.width.equalTo(40)
+                make.height.equalTo(40)
             }
         } catch(let error) {
             print(error.localizedDescription)
--- Sources/DisplayLabel/DisplayLabel.swift
+++ Sources/DisplayLabel/DisplayLabel.swift
@@ -7,43 +7,60 @@
 
 import UIKit
 
-class DisplayLabel: UILabel {
+class DisplayLabel: UIView {
     
     let configure: DisplayLabelConfigure
-    
+        
     init(configure: DisplayLabelConfigure) {
         self.configure = configure
         super.init(frame: CGRect())
-        self.text = configure.contentString
-        self.textColor = configure.contentColor.color
-        self.textAlignment = configure.textAlignment
-        self.adjustsFontSizeToFitWidth = true
-        self.baselineAdjustment = .alignCenters
-        self.numberOfLines = 1
-        self.font = UIFont.systemFont(ofSize: 17)
+        self.backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
         self.configure = DisplayLabelConfigure()
         super.init(coder: coder)
     }
+    
+    override func draw(_ rect: CGRect) {
+        
+        guard rect.size != .zero else {
+            return
+        }
+        
+        let adjustFontAndSize = self.computeSuitableFont(for: rect.size)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = self.configure.textAlignment
+        var textAttributs: [NSAttributedString.Key : Any] = [:]
+        textAttributs[.font] = adjustFontAndSize.0
+        textAttributs[.foregroundColor] = self.configure.contentColor.color
+        textAttributs[.paragraphStyle] = paragraphStyle
+        
+        let stringRect = CGRect(x: rect.origin.x, y: rect.origin.y + (rect.height - adjustFontAndSize.1.height) / 2, width: rect.width, height: adjustFontAndSize.1.height)
+        
+        let nsString = self.configure.contentString as NSString
+        nsString.draw(in: stringRect, withAttributes: textAttributs)
+        
+    }
 }
 
 extension DisplayLabel {
-    func computeSuitableFont(for size: CGSize) -> UIFont {
-        var newFont = self.font!
+    func computeSuitableFont(for size: CGSize) -> (UIFont, CGSize) {
+        var newFont = UIFont.systemFont(ofSize: 17)
         var fontSize = newFont.pointSize
         let nsstring = self.configure.contentString as NSString
         var rect = nsstring.boundingRect(with: CGSize(width: CGFloat(MAXFLOAT), height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : newFont], context: nil)
         while rect.height > size.height || rect.width > size.width {
             if fontSize <= 0.5 {
-                return UIFont.systemFont(ofSize: 0.1)
+                fontSize = 0.1
+                rect = nsstring.boundingRect(with: CGSize(width: CGFloat(MAXFLOAT), height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : newFont], context: nil)
+                return (UIFont.systemFont(ofSize: fontSize), rect.size)
             }
             fontSize -= 0.5
             newFont = UIFont.systemFont(ofSize: fontSize)
             rect = nsstring.boundingRect(with: CGSize(width: CGFloat(MAXFLOAT), height: CGFloat(MAXFLOAT)), options: .usesLineFragmentOrigin, attributes: [NSAttributedString.Key.font : newFont], context: nil)
         }
-        return newFont
+        return (newFont, rect.size)
     }
 }
 
