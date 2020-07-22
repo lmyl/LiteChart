9bed17c522c76626ec25797f75101831ba911156
刘洋 <1269458422@qq.com>
2020-07-22 22:12:02 +0800

    feature(测试):增加检测内存泄漏的测试代码
    增加检测内存泄漏的测试代码

--- LiteChart/ViewController.swift
+++ LiteChart/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    
+    var displayView: LiteChartView?
+    var showType = 1
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +29,7 @@ class ViewController: UIViewController {
         
         let backgroundView = try! LiteChartView(configure: configure)
         self.view.addSubview(backgroundView)
+        self.displayView = backgroundView
         backgroundView.snp.updateConstraints{
             make in
             make.width.equalToSuperview()
@@ -33,8 +37,49 @@ class ViewController: UIViewController {
             make.height.equalTo(300)
         }
         
+        let button = UIButton()
+        self.view.addSubview(button)
+        button.snp.updateConstraints{
+            make in
+            make.width.equalTo(100)
+            make.height.equalTo(50)
+            make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left)
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top)
+        }
+        button.addTarget(self, action: #selector(action), for: .touchUpInside)
+        button.backgroundColor = .yellow
     }
 
+    @objc func action() {
+        var configure: LiteChartViewParameters
+        if self.showType == 1 {
+            self.showType = 0
+            configure = LiteChartViewParameters(inputDatas: .funal(inputDatas: [(80, .init(lightUIColor: .yellow)), (40, .init(lightUIColor: .systemPink))]))
+        } else {
+            self.showType = 1
+            configure = LiteChartViewParameters(inputDatas: .pie(inputDatas: [(80, .init(lightUIColor: .yellow)), (40, .init(lightUIColor: .systemPink))]))
+        }
+        configure.coupleTitles = ["0月", "1月", "2月", "3月", "4月", "5月"]
+        configure.radarCount = 5
+        configure.inputLegendTitles = ["2018", "2019"]
+        configure.titleString = "年度绩效总结"
+        configure.isShowCoupleTitles = true
+        configure.isShowLegendTitles = true
+        configure.displayDataMode = .percent
+        
+        let backgroundView = try! LiteChartView(configure: configure)
+        if let display = self.displayView {
+            display.removeFromSuperview()
+        }
+        self.view.addSubview(backgroundView)
+        self.displayView = backgroundView
+        backgroundView.snp.updateConstraints{
+            make in
+            make.width.equalToSuperview()
+            make.center.equalToSuperview()
+            make.height.equalTo(300)
+        }
+    }
 
 }
 
