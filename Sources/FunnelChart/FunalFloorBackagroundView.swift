46e831ee1b9fb398df58cc3a867dd4ec0ec74fe5
刘洋 <1269458422@qq.com>
2020-09-27 14:22:19 +0800

    feature(漏斗图):漏斗图实现异步绘制
    漏斗图实现异步绘制

--- Sources/FunnelChart/FunalFloorBackagroundView.swift
+++ Sources/FunnelChart/FunalFloorBackagroundView.swift
@@ -15,39 +15,48 @@ class FunalFloorBackagroundView: UIView {
     init(configure: FunalFloorBackagroundViewConfigure) {
         self.configure = configure
         super.init(frame: CGRect())
-        self.backgroundColor = .clear
     }
     
     required init?(coder: NSCoder) {
         self.configure = .emptyConfigure
         super.init(coder: coder)
-        self.backgroundColor = .clear
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         
-        setNeedsDisplay()
+        layer.setNeedsDisplay()
     }
     
-    override func draw(_ rect: CGRect) {
-        let context = UIGraphicsGetCurrentContext()
-        context?.clear(rect)
-        context?.setAllowsAntialiasing(true)
-        context?.setShouldAntialias(true)
-        let topLeft = rect.width * (1 - self.configure.topPercent) / 2
-        let topStartX = rect.origin.x + topLeft
-        let topEndX = rect.origin.x + rect.width - topLeft
-        let bottomLeft = rect.width * (1 - self.configure.bottomPercent) / 2
-        let bottomStartX = rect.origin.x + bottomLeft
-        let bottomEndX = rect.origin.x + rect.width - bottomLeft
-        let topLeftPoint = CGPoint(x: topStartX, y: rect.origin.y)
-        let topRightPoint = CGPoint(x: topEndX, y: rect.origin.y)
-        let bottomLeftPoint = CGPoint(x: bottomStartX, y: rect.origin.y + rect.height)
-        let bottomRightPoint = CGPoint(x: bottomEndX, y: rect.origin.y + rect.height)
-        context?.addLines(between: [topLeftPoint, topRightPoint, bottomRightPoint, bottomLeftPoint, topLeftPoint])
-        context?.setFillColor(self.configure.color.color.cgColor)
-        context?.drawPath(using: .fill)
+    override func display(_ layer: CALayer) {
+        LiteChartDispatchQueue.asyncDrawQueue.async {
+            layer.contentsScale = UIScreen.main.scale
+            UIGraphicsBeginImageContextWithOptions(layer.bounds.size, false, layer.contentsScale)
+            let context = UIGraphicsGetCurrentContext()
+            let rect = layer.bounds
+            context?.clear(rect)
+            context?.setAllowsAntialiasing(true)
+            context?.setShouldAntialias(true)
+            let topLeft = rect.width * (1 - self.configure.topPercent) / 2
+            let topStartX = rect.origin.x + topLeft
+            let topEndX = rect.origin.x + rect.width - topLeft
+            let bottomLeft = rect.width * (1 - self.configure.bottomPercent) / 2
+            let bottomStartX = rect.origin.x + bottomLeft
+            let bottomEndX = rect.origin.x + rect.width - bottomLeft
+            let topLeftPoint = CGPoint(x: topStartX, y: rect.origin.y)
+            let topRightPoint = CGPoint(x: topEndX, y: rect.origin.y)
+            let bottomLeftPoint = CGPoint(x: bottomStartX, y: rect.origin.y + rect.height)
+            let bottomRightPoint = CGPoint(x: bottomEndX, y: rect.origin.y + rect.height)
+            context?.addLines(between: [topLeftPoint, topRightPoint, bottomRightPoint, bottomLeftPoint, topLeftPoint])
+            context?.setFillColor(self.configure.color.color.cgColor)
+            context?.drawPath(using: .fill)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            context?.restoreGState()
+            UIGraphicsEndImageContext()
+            LiteChartDispatchQueue.asyncDrawDoneQueue.async {
+                layer.contents = image?.cgImage
+            }
+        }
     }
     
 }
